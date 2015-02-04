5b8cb77fd941028c8209fb2cc857a00577e29572
Jordan Terry <jordan@nice.agency>
2015-02-04 12:01:45 +0000

    Created a simple OnItemClickListener and Fragment, then an Espresso test.

---
+++ app/src/androidTest/java/test/nice/testproject/NavigationDrawerTests.java
@@ -0,0 +1,49 @@
+package test.nice.testproject;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.suitebuilder.annotation.SmallTest;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.contrib.DrawerActions.openDrawer;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+public class NavigationDrawerTests extends ActivityInstrumentationTestCase2<MainActivity> {
+    private static String TAG = NavigationDrawerTests.class.getSimpleName();
+
+    public NavigationDrawerTests() {
+        super(MainActivity.class);
+    }
+
+    @Override
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        getActivity();
+    }
+
+    @Override
+    @After
+    public void tearDown() throws Exception {
+        super.tearDown();
+
+    }
+
+    /**
+     * Test that clicking on a Navigation Drawer Item will open the correct fragment.
+     */
+    @SmallTest
+    public void testNavigationDrawerItemClick() {
+        openDrawer(R.id.my_drawer_layout);
+        onView(withText("Menu One")).perform(click());
+        onView(allOf(withId(R.id.exampleFragmentText), withText("Menu One"))).check(matches(isDisplayed()));
+    }
+
+}
